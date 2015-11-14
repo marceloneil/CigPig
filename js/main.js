5969d4aef3955a19b59e4cb02f6993e0b8926635
Marcel O'Neil <marceloneil5@gmail.com>
2015-11-14 13:28:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
+        sectionsColor: ['#2C3E50', '#E74C3C', '#ECF0F1', '#3498DB'],
         anchors:['main', 'about', 'how', 'contact']
     });
 });
