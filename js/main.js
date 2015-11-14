9317bf44d887462bd2947a09012b7aa0b15f31c8
Marcel O'Neil <marceloneil5@gmail.com>
2015-11-14 14:08:27 -0500

    Initial

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        sectionsColor: ['#27ae60', '#ecf0f1', '#2ecc71', '#d35400'],
-        anchors:['main', 'about', 'how', 'contact']
-    });
-});
